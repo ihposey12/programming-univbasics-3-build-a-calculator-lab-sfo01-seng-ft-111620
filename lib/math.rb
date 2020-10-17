8930e4f810f20bc33816a407b56c42dba5954b92
static-tutorial-1700 <ihposey@crimson.ua.edu>
2020-10-17 22:52:22 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,27 @@
 def addition(num1, num2)
-
+  sum = num2 + num1
+  return sum
 end
 
 def subtraction(num1, num2)
-
+  difference = num2 - num1
+  return difference
 end
 
 def division(num1, num2)
-
+  divide = num2 / num1
+  return divide
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
+  return product
 end
 
 def modulo(num1, num2)
-
+  
 end
 
 def square_root(num)
-
+  
 end
